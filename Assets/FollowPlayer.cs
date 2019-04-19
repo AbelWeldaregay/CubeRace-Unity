af979cb1740d7a5e9ea5f30c9d7a86ef5e5863e9
Abel Weldaregay <abelweldaregay@gmail.com>
2019-04-19 15:43:23 -0400

    first commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        //adding the player position vector and the offset vector
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+
+    //marked this as "Fixed"Update becasue
+    //we are using it to mess with physics  
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if( Input.GetKey("e") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+
+    }
+}
